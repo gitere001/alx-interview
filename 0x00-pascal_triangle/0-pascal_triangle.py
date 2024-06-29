c2b4e12d57f9787f7a122b139afaf3b3578246d3
James gitere <sasmuypwa2019.com>
2024-06-29 07:11:59 +0300

    updated pycode style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Model having pascal triangle"""
+
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing Pascal’s triangle of n.
     """
     if n <= 0:
         return []
-    
+
     triangle = []
-    
+
     for i in range(n):
         row = [1]  # First element is always 1
         if i > 0:  # Starting from the second row
@@ -17,6 +19,5 @@ def pascal_triangle(n):
                 row.append(last_row[j-1] + last_row[j])
             row.append(1)  # Last element is always 1
         triangle.append(row)
-    
-    return triangle
 
+    return triangle
